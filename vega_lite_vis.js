d23f4fb0afb9291eaa0cf9acc629e8abce6f7a1c
Min0164 <96381348+Min0164@users.noreply.github.com>
2022-10-13 06:40:08 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,11 +1,11 @@
-var vg_1 = "map/choropleth_map.json";
+var vg_1 = "choropleth_map.json";
 vegaEmbed("#choropleth_map", vg_1,{"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 
-var vg_2 = "raw_line/raw_line.json";
+var vg_2 = "raw_line.json";
 vegaEmbed("#raw_line", vg_2,{"actions": false}).then(function(result) {
 
 }).catch(console.error);
@@ -26,7 +26,7 @@ vegaEmbed("#scatter_plot", vg_5,{"actions":false}).then(function(result) {
 
 }).catch(console.error);
 
-var vg_6 = "bubble_chart/gdp.json";
+var vg_6 = "gdp.json";
 vegaEmbed("#scatter_plot2", vg_6,{"actions":false}).then(function(result) {
 
 }).catch(console.error);
