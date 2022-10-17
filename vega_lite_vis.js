910a160e62fc4928adb9b2a4bc068501228001b8
Min0164 <96381348+Min0164@users.noreply.github.com>
2022-10-17 20:09:22 +0800

    uploaded files

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,35 +1,41 @@
-var vg_1 = "choropleth_map.json";
-vegaEmbed("#choropleth_map", vg_1,{"actions": false}).then(function(result) {
+var vg_1 = "map_raw_line.json";
+vegaEmbed("#map_line", vg_1,{"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 
-var vg_2 = "raw_line.json";
-vegaEmbed("#raw_line", vg_2,{"actions": false}).then(function(result) {
-
-}).catch(console.error);
 
 var vg_3 = "bump_chart.json";
 vegaEmbed("#bump_chart", vg_3,{"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
-var vg_4 = "heat_map.json";
-vegaEmbed("#heat_map", vg_4,{"actions":false}).then(function(result) {
+var vg_4 = "gdp_vs_suicide.json";
+vegaEmbed("#bubble_plot1", vg_4,{"actions":false}).then(function(result) {
 
 }).catch(console.error);
 
 
-var vg_5 = "gdp_suicide.json";
-vegaEmbed("#scatter_plot", vg_5,{"actions":false}).then(function(result) {
+var vg_5 = "gdpvsHDI.json";
+vegaEmbed("#bubble_plot2", vg_5,{"actions":false}).then(function(result) {
 
 }).catch(console.error);
 
-var vg_6 = "gdp.json";
-vegaEmbed("#scatter_plot2", vg_6,{"actions":false}).then(function(result) {
+var vg_6 = "population_pyramid.json";
+vegaEmbed("#population_pyramid", vg_6,{"actions":false}).then(function(result) {
 
 }).catch(console.error);
 
 
+var vg_7 = "line_chart_age.json";
+vegaEmbed("#line_chart", vg_7,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_8 = "connecte_dot_plot.json";
+vegaEmbed("#connected_dot", vg_8,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
 
