3d1aa2c176c24eb3f00c046378593bb0486b6401
Min0164 <96381348+Min0164@users.noreply.github.com>
2022-10-18 07:48:09 +0800

    update files

---
+++ data/vega_lite_vis.js
@@ -0,0 +1,41 @@
+var vg_1 = "map_raw_line.json";
+vegaEmbed("#map_line", vg_1,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
+
+var vg_3 = "bump_chart.json";
+vegaEmbed("#bump_chart", vg_3,{"actions": false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "gdp_vs_suicide.json";
+vegaEmbed("#bubble_plot1", vg_4,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_5 = "gdpvsHDI.json";
+vegaEmbed("#bubble_plot2", vg_5,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "population_pyramid.json";
+vegaEmbed("#population_pyramid", vg_6,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_7 = "line_chart_age.json";
+vegaEmbed("#line_chart", vg_7,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_8 = "connecte_dot_plot.json";
+vegaEmbed("#connected_dot", vg_8,{"actions":false}).then(function(result) {
+
+}).catch(console.error);
+
